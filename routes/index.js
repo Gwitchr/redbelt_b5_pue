73d9abea6df3340d00748df619ca38f48441ca51
casillasluisn12 <luiscasillas@n12.mx>
2019-11-14 21:27:05 -0600

    change initial message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('API si manejo, encentra la documentación en api.simanejo.com/documentacion' );
+  res.send('API de prueba' );
 });
 
 module.exports = router;
